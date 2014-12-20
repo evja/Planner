e44d4311ef2925327dc473c24ae6cf2c8067ad99
Lindsay Hammon <lindsay.work.mail@gmail.com>
2014-12-20 14:37:41 -0700

    addedd sorting methods

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,13 @@ class Task < ActiveRecord::Base
 	validates :title,  presence: true, length: { maximum: 20 }
 	validates :description, length: { minimum: 5 }, allow_blank: true
 
+	def self.cleanup
+    @tasks = user.tasks.all
+    @tasks.each do |task|
+	    if task.complete_date
+	      task.destroy if task.complete_date < 30.days.ago
+	    end
+    end
+	end
+
 end
