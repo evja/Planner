52e8e18b7da32bf6d3e938a4d62a0d7fb3a86671
Lindsay Hammon <lindsay.work.mail@gmail.com>
2014-12-20 14:54:35 -0700

    migrated ne cols on task table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141220184811) do
+ActiveRecord::Schema.define(version: 20141220204825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20141220184811) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "score"
+    t.integer  "task_total",             default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
