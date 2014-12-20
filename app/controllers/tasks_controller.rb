83572f8c3aa8a65d673d9f4746505ac3178f4cc1
Konrad Maurer <kradjr@gmail.com>
2014-12-20 11:20:35 -0700

    Updated view functionality.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,7 +16,7 @@ before_action :set_task, only: [:show, :edit, :update, :destroy]
     @task = Task.new(task_params)
     if @task.save
       flash[:success] = "Task #{@task.title} created."
-      redirect_to @tasks
+      redirect_to @task
     else
       render 'new'
     end
@@ -26,12 +26,12 @@ before_action :set_task, only: [:show, :edit, :update, :destroy]
   end
 
   def update
-    if @task.update_atributes(task_params)
+    if @task.update_attributes(task_params)
       if @task.is_completed?
         @task.completed_at = Date.today
         @task.save
       end
-      redirect_to @tasks
+      redirect_to tasks_path
     else
       flash[:danger] = "Task creation faild"
       render 'edit'
@@ -42,14 +42,14 @@ before_action :set_task, only: [:show, :edit, :update, :destroy]
      @task = Task.find(params[:id])
      @task.delete
 
-     redirect_to root_path
+     redirect_to tasks_path
   end
 
 
 private
 
   def set_task
-	 @task = task.find(task_params[:id])
+	 @task = Task.find(params[:id])
   end
 
   def task_params
