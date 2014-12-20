8159be3f73f5c2ec68afa26bf13db35400f60792
Konrad Maurer <kradjr@gmail.com>
2014-12-20 13:02:50 -0700

    misc

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ActiveRecord::Base
 	validates :title,  presence: true, length: { maximum: 20 }
 	validates :description, length: { minimum: 5 }, allow_blank: true
 
+  belongs_to :user
 end
