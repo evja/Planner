17a9a913d4ce5d4e01cdb57121d86446fc4593b0
Evan A <evjay.a@gmail.com>
2014-12-20 10:54:26 -0700

    touched up the controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
   	@tasks = Task.all
   end
@@ -15,7 +16,7 @@ before_action :set_task, only: [:show, :edit, :update, :destroy]
     @task = Task.new(task_params)
     if @task.save
       flash[:success] = "Task #{@task.title} created."
-      redirect_to root_url
+      redirect_to @tasks
     else
       render 'new'
     end
@@ -40,23 +41,21 @@ before_action :set_task, only: [:show, :edit, :update, :destroy]
   def destroy
      @task = Task.find(params[:id])
      @task.delete
-     redirect_to root_url
-  end
-    
+     redirect_to @tasks
   end
 
 
-  private
+private
 
-    def set_task
-  	 @task = task.find(task_params[:id])
-    end 
-    
-    def task_params
-      params.require(:task).permit(:title, :description, :due_date,
-                                   :is_complete, :complete_date )
-    end
- 
+  def set_task
+	 @task = task.find(task_params[:id])
   end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :due_date,
+                                 :is_complete, :complete_date )
+  end
+
+end
 
 
