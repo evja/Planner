a7019439e0920aebf5875be57ebcf7a40afd2c99
Lindsay Hammon <lindsay.work.mail@gmail.com>
2014-12-20 10:36:23 -0700

    updated the update action in the task controller and added a create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,38 @@ before_action :set_task, only: [:show, :edit, :update, :destroy]
     @task = Task.new
   end
 
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = "Task #{@task.title} created."
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
   def edit
+  end
+
+  def update
+    if @task.update_atributes(task_params)
+      if @task.is_completed?
+        @task.completed_at = Date.today
+        @task.save
+      end
+      redirect_to @task
+    else
+      flash[:danger] = "Task creation faild"
+      render 'edit'
+    end
+  end
+
+  def destroy
+     @task = Task.find(params[:id])
+     @task.delete
+     redirect_to root_url
+  end
+    
   end
 
 
